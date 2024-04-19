7275da1f77a280858822910d0858609d0f45ffce
Priti Gupta <163376375+Pritideveloper1012@users.noreply.github.com>
2024-04-19 11:50:06 +0530

    Add files via upload

---
+++ EmployeeCrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
